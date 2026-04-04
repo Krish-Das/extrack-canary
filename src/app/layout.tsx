f301aff88adcd0fb0fd6df3c14740afb94d53d89
Krish-Das <krishdasatgames@gmail.com>
2026-04-04 15:21:12 +0530

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,7 @@ import "./globals.css"
 
 import Provider from "@/components/provider"
 
-console.log
-;("cause error here!")
-)
+console.log("cause error here!")
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
